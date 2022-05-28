633f1700a7d63d75ece867b8519189de599f5a0e
tomasitobambito <tomgacek0604@gmail.com>
2022-05-29 01:36:57 +0200

    removed traces of test endpoints

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import BingoTileRouter from "./bingotile/bingotile.routes";
-import TeamsRoutes from "./teams/teams.routes";
 
 const router = Router();
 
-router.use('/teams', TeamsRoutes)
 router.use('/bingotile', BingoTileRouter)
 
 export default router;
