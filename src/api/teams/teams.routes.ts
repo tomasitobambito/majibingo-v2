0e7ba7ab20f01c807e131fe86271ed87df8ebe3e
tomasitobambito <73672292+tomasitobambito@users.noreply.github.com>
2022-05-29 01:36:08 +0200

    Delete temp files

--- src/api/teams/teams.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getTeams } from "./teams.controller";
-
-const TeamsRoutes = Router();
-
-TeamsRoutes.route('/').get(getTeams);
-
-export default TeamsRoutes;
